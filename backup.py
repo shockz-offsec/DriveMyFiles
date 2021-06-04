a73a89b08edef4c1178e3986a5ca57e9680393b7
Jorge Manuel Lozano Gómez <jmlgomez73@gmail.com>
2021-06-05 01:15:12 +0200

    Created logging functionality, Implemented message logs in backup.py, changes in config.json
    In order to use the logger (funtion to write log messages into a log file):
         "import this : from logger_settings import logger"
    After that just call the function wherever you want like this:
         " logger.info("text") "
    This function counts with this types of message : info, error, warning, critical and debug.
    
    Moreover two parameters was removed from the json file: TEMP_DIR and LOG_DIR.
    It was considered that this routes are static so the user should not be required to specify a path for these directories.

--- backup.py
+++ backup.py
@@ -3,6 +3,7 @@ import json
 import shutil
 from distutils.dir_util import copy_tree
 import zipfile
+from logger_settings import logger
 
 """Write into the json file"""
 def writing():
@@ -14,17 +15,18 @@ def writing():
 """Compress all the files recursively into a zipfile located in the temp dir"""
 def compress():
 
-    json_data = json.load(open('config.json','r'))
     zp_name = 'backupdrive.zip'
-    zp_path = json_data["GENERAL"]["TEMP_DIR"] + '\\' + zp_name
+    zp_path = 'temp\\' + zp_name
     zp_file =  zipfile.ZipFile(zp_path, 'w')
- 
-    for root, subfolders, files in os.walk(json_data["GENERAL"]["TEMP_DIR"]):
+    
+    logger.info("Compressing files ...")
+    for root, subfolders, files in os.walk('temp'):
         for filename in files:
             if filename != zp_name:
-                zp_file.write(os.path.join(root, filename), os.path.relpath(os.path.join(root,filename), json_data["GENERAL"]["TEMP_DIR"]), compress_type = zipfile.ZIP_DEFLATED)
+                zp_file.write(os.path.join(root, filename), os.path.relpath(os.path.join(root,filename), 'temp'), compress_type = zipfile.ZIP_DEFLATED)
     
     zp_file.close()
+    logger.info("All files compressed")
 
 """Unzip the zip file downloaded from google drive
 Args: 
@@ -32,22 +34,30 @@ Args:
     dest_dir: destination path
 """
 def unzip(zp_path,dest_dir):
-    zp_file = zipfile.ZipFile(zp_path)
+    logger.info("Unzipping...")
+    try:
+        zp_file = zipfile.ZipFile(zp_path)
+    except:
+        logger.error("File could not be opened")
     zp_file.extractall(dest_dir)
     zp_file.close()
+    logger.info("Unzip completed")
 
 
 """Clean the temp directory
 Args:
     temp_dir: temp's path
+    
+Call it after upload the files to Google Drive
 """
 def clean(temp_dir):
+    logger.info("Cleaning temp files...")
     try:
         shutil.rmtree(temp_dir, ignore_errors=False, onerror=None)
         os.makedirs(temp_dir)
     except OSError as e:
-        print("Error:" + e.strerror)
-    
+        logger.error("Could not delete the directory - " + e.strerror)
+    logger.info("Complete cleaning")
 
 """Extracts all the information from the directories specified in the configuration file, copying them to the temporary directory
 Args:
@@ -58,19 +68,23 @@ def recompile(make_compression=True):
     json_data = json.load(open('config.json','r'))
     lista = list(json_data["DIRECTORIES"])
     
-    if not os.path.exists(json_data["GENERAL"]["TEMP_DIR"]):
-        os.makedirs(json_data["GENERAL"]["TEMP_DIR"])
-
+    if not os.path.exists('temp'):
+        os.makedirs('temp')
+    logger.info("Copying files...")
     for ruta in lista:
         if os.path.exists(ruta):
                 if os.path.isdir(ruta):
-                    copy_tree(str(ruta), json_data["GENERAL"]["TEMP_DIR"])
+                    copy_tree(str(ruta), 'temp')
                 else:
-                    shutil.copy2(str(ruta), json_data["GENERAL"]["TEMP_DIR"])
+                    shutil.copy2(str(ruta), 'temp')
     if(make_compression):
         compress()
-
     
 if __name__ == "__main__":
-    #recompile()
-    clean("C:\\Users\\Shockz\\Desktop\\MyBackupDrive\\temp")
+    # execute this at the start of the script
+    if not os.path.exists('temp'):
+        os.makedirs('temp')
+    
+    # Test
+    recompile()
+    
---
+++ logger_settings.py
@@ -0,0 +1,41 @@
+import sys
+import logging
+from logging.config import dictConfig
+
+logging_config = dict(
+    version=1,
+    formatters={
+        'verbose': {
+            'format': ("[%(asctime)s] %(levelname)s "
+                       "[%(name)s:%(lineno)s] %(message)s"),
+            'datefmt': "%d/%b/%Y %H:%M:%S",
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s',
+        },
+    },
+    handlers={
+        'logger': {'class': 'logging.handlers.RotatingFileHandler',
+                           'formatter': 'verbose',
+                           'level': logging.DEBUG,
+                           'filename': 'log/message.log',
+                           'maxBytes': 52428800,
+                           'backupCount': 7},
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'stream': sys.stdout,
+        },
+    },
+    loggers={
+        'logger': {
+            'handlers': ['logger', 'console'],
+            'level': logging.DEBUG
+        },
+    }
+)
+
+dictConfig(logging_config)
+
+logger = logging.getLogger('logger')
